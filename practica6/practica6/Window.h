2c43cff5693adeaf13483a95a1ae698cf7fc6281
Josue Ramirez <josue1999s@hotmail.com>
2021-07-09 14:16:41 -0500

    codigo

---
+++ practica6/practica6/Window.h
@@ -0,0 +1,49 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	GLfloat getmuevey() { return muevey; }
+	GLfloat getmuevez() { return muevez; }
+
+	GLfloat getmuevecarrox() { return muevecarrox; }
+	GLfloat getmuevecarroz() { return muevecarroz; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	GLfloat muevey;
+	GLfloat muevez;
+
+	GLfloat muevecarrox;
+	GLfloat muevecarroz;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
